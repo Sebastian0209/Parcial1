20a72ef14953e36442c1aa9d45dead0447f7923c
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-03-30 12:06:21 -0400

    Commit MVC

---
+++ Parcial/MVC Parcial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Parcial.Startup))]
+namespace MVC_Parcial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
