1629b2b89f49e30c5eabc53479c6f32b6ceb7f5c
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-03-30 12:32:58 -0400

    Commit con modelo MVC

---
+++ Parcial/MVC Parcial/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVC_Parcial.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVC_Parcial.Models.Product> Products { get; set; }
+    }
+}
